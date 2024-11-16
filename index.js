b88c2a0dc602cac843ad706633ac03b59baf45c6
Marko Damijanjević <markodamijanjevic1@gmail.com>
2024-11-16 19:46:27 +0100

    Implementacija POST HTPP metode za narucivanje narudzbe

--- index.js
+++ index.js
@@ -1,37 +1,74 @@
-const express = require("express");
-const path = require("path");
+import express from "express";
 const app = express();
+app.use(express.json());
 const PORT = 3000;
 
-let users = [
-  {
-    id: 1,
-    ime: "Marko",
-    prezime: "Damijanjević",
-  },
-  {
-    id: 2,
-    ime: "Ivo",
-    prezime: "Ivić",
-  },
-  {
-    id: 3,
-    ime: "Loren",
-    prezime: "Tadić",
-  },
+let narudzbe = [];
+
+const pizze = [
+  { id: 1, naziv: "Margherita", cijena: 6.5 },
+  { id: 2, naziv: "Capricciosa", cijena: 8.0 },
+  { id: 3, naziv: "Quattro formaggi", cijena: 10.0 },
+  { id: 4, naziv: "Šunka sir", cijena: 7.0 },
+  { id: 5, naziv: "Vegetariana", cijena: 9.0 },
 ];
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "Zad 1.html"));
+app.post("/naruci", (req, res) => {
+  let ukupna_cijena = 0;
+  const { narudzba, klijent } = req.body;
+  if (!Array.isArray(narudzba)) {
+    res.json({ message: "Ocekujem polje objekata!" });
+    return;
+  }
+
+  const klj_klijenta = Object.keys(klijent);
+  if (
+    !(
+      klj_klijenta.includes("prezime") &&
+      klj_klijenta.includes("adresa") &&
+      klj_klijenta.includes("broj_telefona")
+    )
+  ) {
+    res.json({ message: "Niste poslali sve podatke o klijentu za narudznu!" });
+    return;
+  }
+
+  for (let item of narudzba) {
+    const kljucevi = Object.keys(item);
+    if (
+      !(
+        kljucevi.includes("pizza") &&
+        kljucevi.includes("velicina") &&
+        kljucevi.includes("kolicina")
+      )
+    ) {
+      res.json({ message: "Niste poslali sve podatke za narudzbu!" });
+      return;
+    }
+
+    let pizza = pizze.find((pizza) => pizza.naziv == item.pizza);
+    if (!pizza) {
+      res.json({
+        message: `Ta pizza ${item.pizza} ne postoji u nasem jelovniku!`,
+      });
+      return;
+    }
+    ukupna_cijena += pizza.cijena * item.kolicina;
+  }
+  narudzbe.push({ narudzba, klijent, ukupna_cijena });
+
+  const pizze_ime_vel = narudzba
+    .map((item) => `${item.pizza} ${item.velicina}`)
+    .join(", ");
+
+  res.json({
+    message: `Vaša narudžba za ${pizze_ime_vel} je uspješno zaprimljena!`,
+    prezime: klijent.prezime,
+    adresa: klijent.adresa,
+    ukupna_cijena: ukupna_cijena,
+  });
 });
 
-app.get("/about", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "Zad 2.html"));
-});
-
-app.get("/users", (req, res) => {
-  res.json(users);
-});
 app.listen(PORT, (error) => {
   if (error) {
     console.log("Doslo je do greske prilikom pokretannja web posluzitelja");
